6994cccdafdba272adb7d3e4b91d3369fecff576
Matthew Rupp <mattrupp713@gmail.com>
2020-09-10 19:49:19 -0500

    Add validation for required fields

--- client/scripts.js
+++ client/scripts.js
@@ -1,8 +1,21 @@
 const nav = document.querySelector('nav')
+const inputs = document.querySelectorAll('input')
 
 document.querySelector('.fa-hamburger').addEventListener('click',
 // Callback fxn.
 () => {
   // Short-circuit OR operator
   nav.classList.replace("is-collapsed", "is-expanded") || nav.classList.replace("is-expanded", "is-collapsed")
+})
+
+inputs.forEach(input => {
+  input.addEventListener('blur', (event) => {
+    const currentInput = event.target
+
+    if (!currentInput.value) {
+      currentInput.classList.replace("is-success", "is-error") || currentInput.classList.add("is-error")
+    } else {
+      currentInput.classList.replace("is-error", "is-success") || currentInput.classList.add("is-success")
+    }
+  })
 })
